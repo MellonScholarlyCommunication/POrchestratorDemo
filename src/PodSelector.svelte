d4791879dbef0416f0a1bea872938ea61c0534be
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2021-10-04 13:25:06 +0200

    Skipping non existing cards

--- src/PodSelector.svelte
+++ src/PodSelector.svelte
@@ -7,6 +7,8 @@
 <select bind:value="{pod}">
     <option>Choose a pod</option>
 {#each $cardList as card}
+   {#if card}
     <option value={card}>{card.name.toUpperCase()}</option>
+   {/if}
 {/each}
 </select>
