39d4f250ec3863104ff52bb4e90635178874d763
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2021-10-28 14:47:16 +0200

    Adding Subtype

---
+++ src/notification/SubType.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let as2SubType;
+
+    let as2SubTypes = [
+        'ArchivationAction' ,
+        'AwarenessAction'   ,
+        'CertificationAction' ,
+        'EndorsementAction',
+        'RegistrationAction' 
+    ];
+</script>
+
+<b>SubType</b><br>
+<select bind:value={as2SubType}>
+        <option></option>
+    {#each as2SubTypes as t}
+        <option value={t}>{t}</option>
+    {/each}
+</select>
